adb0de071e0b7312881c399cfa50b7a537a9664d
Mikko Kokkonen <mikko@owlforestry.com>
2015-03-03 01:20:55 +0800

    Implemented horizontal line

---
+++ lib/commands/hline.rb
@@ -0,0 +1,21 @@
+class HLine < Command
+  register /H (\d+) (\d+) (\d+) ([A-Z])/
+
+  def initialize(x1, x2, y, colour)
+    @x1 = x1.to_i
+    @x2 = x2.to_i
+    @y = y.to_i
+    @colour = colour
+  end
+
+  def execute(app)
+    unless app.bitmap
+      puts "ERR: No bitmap has been created"
+      return
+    end
+
+    (@x1..@x2).each do |x|
+      app.bitmap.data[(@y-1)*app.bitmap.width+(x-1)] = @colour
+    end
+  end
+end
