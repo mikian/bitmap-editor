53c54def20abca4e54dfeae7cb919572da986033
Mikko Kokkonen <mikko@owlforestry.com>
2015-03-03 11:59:05 +0800

    Implemented fill tool

---
+++ lib/commands/fill.rb
@@ -0,0 +1,38 @@
+class Fill < Command
+  register /F (\d+) (\d+) ([A-Z])/
+
+  def initialize(x, y, colour)
+    @x = x.to_i
+    @y = y.to_i
+    @colour = colour
+  end
+
+  def execute(app)
+    unless app.bitmap
+      puts "ERR: No bitmap has been created"
+      return
+    end
+
+    unless app.bitmap.bounds?(@x, @y)
+      puts "ERR: Coordinates out of bounds"
+      return
+    end
+
+    @original_colour = app.bitmap[@x, @y]
+    @bitmap = app.bitmap
+
+    fill(@x, @y)
+  end
+
+  private
+  def fill(x, y)
+    return unless @bitmap.bounds?(x, y) && @bitmap[x, y] == @original_colour
+
+    @bitmap[x, y] = @colour
+
+    fill(x + 1, y)
+    fill(x, y + 1)
+    fill(x - 1, y)
+    fill(x, y - 1)
+  end
+end
