bf3450313f8701e5efa4e5b36a7fcaaed2aa8f69
Mikko Kokkonen <mikko@owlforestry.com>
2015-03-03 11:58:43 +0800

    Added easy accessors for modifying bitmap data

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -7,4 +7,20 @@ class Bitmap
     @height = height
     @data = Array.new(width * height, 'O')
   end
+
+  def bounds?(x, y)
+    x <= @width && y <= @height
+  end
+
+  def [](x, y)
+    @data[(y-1)*@width+(x-1)]
+  end
+
+  def []=(x, y, val)
+    @data[(y-1)*@width+(x-1)] = val
+  end
+
+  def rows
+    @data.each_slice(@width)
+  end
 end
